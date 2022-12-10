30285510b7548eba4f462536608d7b33354c3ea4
Esmanur <102920301+esmayric@users.noreply.github.com>
2022-12-10 17:41:38 +0300

    Update VucutKitleIndeksi.java

--- VucutKitleIndeksi.java
+++ VucutKitleIndeksi.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class VucutKutleIndeksi {
+public class VucutKitleIndeksi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
